8fe49ba218ba3db8532694fcf94f872eabf1451e
davidegalletti99@users.noreply.github.com <davidegalletti99@users.noreply.github.com>
2023-08-24 15:21:38 +0200

    modifiche allo schema tigergraph

--- docker-bigdata-playground/scripts/exec-load-data.sh
+++ docker-bigdata-playground/scripts/exec-load-data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker exec namenode bash ./load-data.sh 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker exec namenode bash -c "bash $(cat $SCRIPT_DIR/load-data.sh)"
