356b1a972cd6519ffdc5e9c30ceef5ceeeac7408
Francesco9932 <72614773+Francesco9932@users.noreply.github.com>
2023-08-26 16:12:17 +0200

    Aggiunto encoding utf-8

--- join_dataset.py
+++ join_dataset.py
@@ -8,11 +8,11 @@ os.makedirs(output_path, exist_ok=True)
 for dir in os.listdir(dataset_path):
     if not os.path.isdir(os.path.join(dataset_path, dir)):
         continue
-    with open(os.path.join(output_path, dir+".json"), "w") as output_file:
+    with open(os.path.join(output_path, dir+".json"), "w", encoding="utf-8") as output_file:
         for file in os.listdir(os.path.join(dataset_path, dir)):
             if not file.endswith(".json"):
                 continue
             print(os.path.join(dataset_path, dir, file))
-            with open(os.path.join(dataset_path, dir, file), "r") as input_file:
+            with open(os.path.join(dataset_path, dir, file), "r", encoding="utf-8") as input_file:
                 for line in input_file:
                     output_file.write(line)
