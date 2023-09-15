280158345c15eb2c6e6f70ef8657ed61bf77b986
Davide Galletti <d.galletti99@gmail.com>
2023-09-15 12:06:16 +0200

    modifiche al join_dataset.py + aggiunte dipendenze per l'API github

--- join_dataset.py
+++ join_dataset.py
@@ -1,16 +1,16 @@
 import os
 
-dataset_path = ""
+dataset_path = "./dataset"
 output_path = "./datasets"
-
+ext = ".csv"
 os.makedirs(output_path, exist_ok=True)
 
 for dir in os.listdir(dataset_path):
     if not os.path.isdir(os.path.join(dataset_path, dir)):
         continue
-    with open(os.path.join(output_path, dir+".json"), "w", encoding="utf-8") as output_file:
+    with open(os.path.join(output_path, dir+ext), "w", encoding="utf-8") as output_file:
         for file in os.listdir(os.path.join(dataset_path, dir)):
-            if not file.endswith(".json"):
+            if not file.endswith(ext):
                 continue
             print(os.path.join(dataset_path, dir, file))
             with open(os.path.join(dataset_path, dir, file), "r", encoding="utf-8") as input_file:
